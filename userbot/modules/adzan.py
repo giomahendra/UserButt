6c35b8f8ff84a7d7928e08a657fa5034a64c39b3
Trojans <44092706+giomahendra@users.noreply.github.com>
2020-09-10 23:59:20 +0700

    Update adzan.py

--- userbot/modules/adzan.py
+++ userbot/modules/adzan.py
@@ -1,182 +1,56 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-"""Userbot module for executing code and terminal commands from Telegram."""
+import json
+import requests
 
-import asyncio
-from os import remove
-from sys import executable
-from userbot import CMD_HELP, BOTLOG, BOTLOG_CHATID, TERM_ALIAS
+from userbot import CMD_HELP
 from userbot.events import register
 
-
-@register(outgoing=True, pattern=r"^\.eval(?: |$|\n)(.*)")
-async def evaluate(query):
-    """For .eval command, evaluates the given Python expression."""
-    if query.is_channel and not query.is_group:
-        return await query.edit("`Eval isn't permitted on channels`")
-
-    if query.pattern_match.group(1):
-        expression = query.pattern_match.group(1)
-    else:
-        return await query.edit("``` Give an expression to evaluate. ```")
-
-    if expression in ("userbot.session", "config.env"):
-        return await query.edit("`That's a dangerous operation! Not Permitted!`")
-
-    try:
-        evaluation = str(eval(expression))
-        if evaluation:
-            if isinstance(evaluation, str):
-                if len(evaluation) >= 4096:
-                    with open("output.txt", "w+") as file:
-                        file.write(evaluation)
-                    await query.client.send_file(
-                        query.chat_id,
-                        "output.txt",
-                        reply_to=query.id,
-                        caption="`Output too large, sending as file`",
-                    )
-                    remove("output.txt")
-                    return
-                await query.edit("**Query: **\n`"
-                                 f"{expression}\n"
-                                 "`\n**Result: **\n`"
-                                 f"{evaluation}"
-                                 "`")
-        else:
-            await query.edit("**Query: **\n`"
-                             f"{expression}"
-                             "`\n**Result: **\n`No Result Returned/False`")
-    except Exception as err:
-        await query.edit("**Query: **\n`"
-                         f"{expression}"
-                         "`\n**Exception: **\n"
-                         f"`{err}`")
-
-    if BOTLOG:
-        await query.client.send_message(
-            BOTLOG_CHATID, f"Eval query {expression} was executed successfully."
-        )
+PLACE = ''
 
 
-@register(outgoing=True, pattern=r"^\.exec(?: |$|\n)([\s\S]*)")
-async def run(run_q):
-    """For .exec command, which executes the dynamically created program"""
-    code = run_q.pattern_match.group(1)
-
-    if run_q.is_channel and not run_q.is_group:
-        return await run_q.edit("`Exec isn't permitted on channels!`")
-
-    if not code:
-        return await run_q.edit("``` At least a variable is required to"
-                                "execute. Use .help exec for an example.```")
-
-    if code in ("userbot.session", "config.env"):
-        return await run_q.edit("`That's a dangerous operation! Not Permitted!`")
-
-    if len(code.splitlines()) <= 5:
-        codepre = code
-    else:
-        clines = code.splitlines()
-        codepre = clines[0] + "\n" + clines[1] + "\n" + clines[2] + \
-            "\n" + clines[3] + "..."
-
-    command = "".join(f"\n {l}" for l in code.split("\n.strip()"))
-    process = await asyncio.create_subprocess_exec(
-        executable,
-        '-c',
-        command.strip(),
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await process.communicate()
-    result = str(stdout.decode().strip()) \
-        + str(stderr.decode().strip())
-
-    if result:
-        if len(result) > 4096:
-            with open("output.txt", "w+") as file:
-                file.write(result)
-            await run_q.client.send_file(
-                run_q.chat_id,
-                "output.txt",
-                reply_to=run_q.id,
-                caption="`Output too large, sending as file`",
-            )
-            remove("output.txt")
+@register(pattern="^.adzan(?: |$)(.*)")
+async def get_adzan(adzan):
+    if not adzan.pattern_match.group(1):
+        LOCATION = PLACE
+        if not LOCATION:
+            await adzan.edit("Please specify a city or a state.")
             return
-        await run_q.edit("**Query: **\n`"
-                         f"{codepre}\n"
-                         "`\n**Result: **\n`"
-                         f"{result}"
-                         "`")
     else:
-        await run_q.client.send_message(
-            BOTLOG_CHATID, "**Query: **\n`" f"{codepre}" "`\n**Result: **\n`No result returned/False`"
-        )
+        LOCATION = adzan.pattern_match.group(1)
 
-    #if BOTLOG:
-        #await run_q.client.send_message(
-            #BOTLOG_CHATID, "Exec query " + codepre + " was executed successfully."
-        #)
+    # url = f'http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc'
+    url = f'https://api.pray.zone/v2/times/today.json?city={LOCATION}'
+    request = requests.get(url)
+    if request.status_code == 500:
+        return await adzan.edit(f"Couldn't find city `{LOCATION}`")
 
+    parsed = json.loads(request.text)
 
-@register(outgoing=True, pattern=r"^\.term(?: |$|\n)(.*)")
-async def terminal_runner(term):
-    """For .term command, runs bash commands and scripts on your server."""
-    curruser = TERM_ALIAS
-    command = term.pattern_match.group(1)
-    try:
-        from os import geteuid
-        uid = geteuid()
-    except ImportError:
-        uid = "This ain't it chief!"
+    city = parsed["results"]["location"]["city"]
+    country = parsed["results"]["location"]["country"]
+    timezone = parsed["results"]["location"]["timezone"]
+    a = parsed["results"]["datetime"][0]["date"]["gregorian"]
+    date = "%s-%s-%s" % (a[8:], a[5:][:-3], a[0:][:-6])
 
-    if term.is_channel and not term.is_group:
-        return await term.edit("`Term commands aren't permitted on channels!`")
+    imsak = parsed["results"]["datetime"][0]["times"]["Imsak"]
+    subuh = parsed["results"]["datetime"][0]["times"]["Fajr"]
+    zuhur = parsed["results"]["datetime"][0]["times"]["Dhuhr"]
+    ashar = parsed["results"]["datetime"][0]["times"]["Asr"]
+    maghrib = parsed["results"]["datetime"][0]["times"]["Maghrib"]
+    isya = parsed["results"]["datetime"][0]["times"]["Isha"]
 
-    if not command:
-        return await term.edit("``` Give a command or use .help term for an example.```")
+    result = (f"**Jadwal Sholat**:\n"
+              f"📅 `{date} | {timezone}`\n"
+              f"📍 `{city} | {country}`\n\n"
+              f"**Imsak :** `{imsak}`\n"
+              f"**Subuh :** `{subuh}`\n"
+              f"**Zuhur :** `{zuhur}`\n"
+              f"**Ashar :** `{ashar}`\n"
+              f"**Maghrib :** `{maghrib}`\n"
+              f"**Isya :** `{isya}`\n")
 
-    if command in ("userbot.session", "config.env"):
-        return await term.edit("`That's a dangerous operation! Not Permitted!`")
-
-    process = await asyncio.create_subprocess_shell(
-        command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await process.communicate()
-    result = str(stdout.decode().strip()) \
-        + str(stderr.decode().strip())
-
-    if len(result) > 4096:
-        with open("output.txt", "w+") as output:
-            output.write(result)
-        await term.client.send_file(
-            term.chat_id,
-            "output.txt",
-            reply_to=term.id,
-            caption="`Output too large, sending as file`",
-        )
-        remove("output.txt")
-        return
-
-    if uid == 0:
-        await term.edit("`" f"{curruser}:~# {command}" f"\n{result}" "`")
-    else:
-        await term.edit("`" f"{curruser}:~$ {command}" f"\n{result}" "`")
-
-    if BOTLOG:
-        await term.client.send_message(
-            BOTLOG_CHATID, "Terminal command " + command + " was executed sucessfully.",
-        )
-
+    await adzan.edit(result)
 
 CMD_HELP.update({
-    "eval": "`.eval 2 + 3`" "\nUsage: Evalute mini-expressions.",
-    "exec": "`.exec print('hello')`" "\nUsage: Execute small python scripts.",
-    "term": "`.term <cmd>`"
-    "\nUsage: Run bash commands and scripts on your server.",
+    "adzan": "`.adzan` <city>"
+    "\nUsage: Gets the prayer time for moslem."
 })
