938bbe9f24e9b509391222bd59283f72224b3e49
Trojans <44092706+giomahendra@users.noreply.github.com>
2020-09-12 02:38:50 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -284,7 +284,7 @@ with bot:
                 )
             elif query.startswith("tb_btn"):
                 result = builder.article(
-                    "Trojans",
+                    "Commands",
                     text="List of Modules",
                     buttons=[],
                     link_preview=True)
