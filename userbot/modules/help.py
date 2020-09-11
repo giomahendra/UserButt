1f76b0895c23dad1bd59786eeb480bc521db1710
Trojans <44092706+giomahendra@users.noreply.github.com>
2020-09-12 03:11:36 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -13,7 +13,7 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^help(?: |$)(.*)")
 async def help(event):
-    args = event.pattern_match.group(1).startswith("help")
+    args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
