5e58945b0e75ab8130d980bf034d31de3c9275ad
Trojans <44092706+giomahendra@users.noreply.github.com>
2020-09-12 02:28:10 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -224,7 +224,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {}".format("🔹", y), data="ub_modul_{}".format(y))
+        custom.Button.inline("{}".format(x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
